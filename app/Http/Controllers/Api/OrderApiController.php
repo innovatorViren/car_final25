f27a50486985d432f2618ab567171d4129d34c9b
virendra <virendra@mnstechnologies.com>
2026-02-23 10:10:03 +0530

    message chanegs doness

--- app/Http/Controllers/Api/OrderApiController.php
+++ app/Http/Controllers/Api/OrderApiController.php
@@ -160,7 +160,6 @@ class OrderApiController extends ApiController
             $dataArray = [
                 'come_from'=>'admin-customer-order',
                 'order_id'=>(string)$model->id,
-                // 'order_date'=>Carbon::parse($model->date)->format('d-m-Y'),
                 'order_name'=>$model->code
             ];
 
@@ -498,7 +497,7 @@ class OrderApiController extends ApiController
                     "body" => $description,
                     // "test" => 'sddsds',
                 ],
-                // "data" =>$dataArray,
+                "data" =>$dataArray,
             ]
         ];
 
