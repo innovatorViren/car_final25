ea4023f3cd7a208dc901c776aa691b8cc83016f3
virendra <virendra@mnstechnologies.com>
2026-03-20 10:11:25 +0530

    issue chanegs

--- database/migrations/2026_03_13_085203_add_code_to_orders_table.php
+++ database/migrations/2026_03_13_085203_add_code_to_orders_table.php
@@ -12,14 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->string('order_id')->nullable()->after('status');
-            $table->string('state')->nullable()->after('order_id');
-            $table->string('payment_status')->nullable()->after('state');
-            $table->string('amount')->nullable()->after('payment_status');
-            $table->string('currency')->nullable()->after('amount');
+            $table->string('merchant_order_id')->nullable()->after('status');
+            $table->string('order_id')->nullable()->after('merchant_order_id');
+            $table->string('payment_status')->nullable()->after('order_id');
+            $table->string('payable_amount')->nullable()->after('payment_status');
+            $table->string('currency')->nullable()->after('payable_amount');
             $table->string('transaction_id')->nullable()->after('currency');
-            $table->string('payable_amount')->nullable()->after('transaction_id');
-            $table->string('payment_mode')->nullable()->after('payable_amount');
+            $table->string('payment_mode')->nullable()->after('transaction_id');
             $table->string('payment_type')->nullable()->after('payment_mode');
             $table->string('payment_time')->nullable()->after('payment_type');
             $table->string('transaction_state')->nullable()->after('payment_time');
@@ -37,13 +36,12 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('merchant_order_id');
             $table->dropColumn('order_id');
-            $table->dropColumn('state');
             $table->dropColumn('payment_status');
-            $table->dropColumn('amount');
+            $table->dropColumn('payable_amount');
             $table->dropColumn('currency');
             $table->dropColumn('transaction_id');
-            $table->dropColumn('payable_amount');
             $table->dropColumn('payment_mode');
             $table->dropColumn('payment_type');
             $table->dropColumn('payment_time');
